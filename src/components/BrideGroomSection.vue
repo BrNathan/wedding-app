2d5f29c3c3a1cb9ee14f5129d8fa718f1da26e3e
Nathan BRUET <nathan.bruet@gmail.com>
2020-09-03 08:44:15 +0200

    bride and groom section

---
+++ src/components/BrideGroomSection.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="ftco-section bg-section">
+    <div class="overlay-top" style="background-image: url(images/top-bg.jpg);"></div>
+    <div class="overlay-bottom" style="background-image: url(images/bottom-bg.jpg);"></div>
+    <div class="container">
+      <div class="row justify-content-center pb-5">
+        <div class="col-md-12 text-center heading-section">
+          <span class="clone">Bride &amp; Groom</span>
+          <h2 class="mb-3">Bride &amp; Groom</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-10">
+          <div class="row">
+            <div class="col-md-6 text-center d-flex align-items-stretch">
+              <div class="bride-groom">
+                <div class="img" style="background-image: url(images/groom.jpg);"></div>
+                <div class="text mt-4 px-4">
+                  <h2>Francisco Fredricksen</h2>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there
+                    live
+                    the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a
+                    large
+                    language ocean.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 text-center d-flex align-items-stretch">
+              <div class="bride-groom">
+                <div class="img" style="background-image: url(images/bride.jpg);"></div>
+                <div class="text mt-4 px-4">
+                  <h2>Laura Moorey</h2>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there
+                    live
+                    the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a
+                    large
+                    language ocean.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {}
+})
+export default class BrideGroomSection extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/WeddingDateSection.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="ftco-section ftco-about ftco-no-pt ftco-no-pb" id="groom-bride-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="wrap">
+            <div class="row d-flex">
+              <div class="col-md-6 d-flex">
+                <div class="img d-flex align-self-stretch align-items-center"
+                  style="background-image:url(images/about.jpg);">
+                </div>
+              </div>
+              <div class="col-md-6 py-md-5 text">
+                <div class="py-md-4">
+                  <div class="row justify-content-start pb-3">
+                    <div class="col-md-12 p-4 p-lg-5 text-center">
+                      <span class="subheading mb-4">Join us to celebrate <br>the wedding day of</span>
+                      <h2 class="mb-4">Francisco <span>&amp;</span> Laura</h2>
+                      <span class="icon flaticon-rose-variant-outline-with-vines"></span>
+                      <span class="subheading">Which is celebration on</span>
+                      <p class="time mb-4"><span>Dec | 28 | 2019</span></p>
+                      <span class="subheading mb-5">Starting at 2:00 <br> in the afternoon</span>
+                      <span class="subheading mb-5">Saint John Paul Church <br> in YorkNew.in</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {}
+})
+export default class WeddingDateSection extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
