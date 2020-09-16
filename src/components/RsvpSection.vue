b2155a2fab26eee6f639a21d93599d4fc4fc5f3f
Nathan BRUET <nathan.bruet@gmail.com>
2020-09-16 07:57:18 +0200

    Rsvp section

---
+++ src/components/RsvpSection.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="ftco-section bg-secondary" id="rsvp-section">
+    <div class="container">
+      <div class="row justify-content-center pb-5">
+        <div class="col-md-12 text-center heading-section">
+          <span class="clone">RSVP</span>
+          <h2 class="mb-3">Are Your Attending?</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-7">
+          <form action="#" class="rsvp-form">
+            <div class="">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Name">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Your email">
+              </div>
+            </div>
+            <div class="">
+              <div class="form-group">
+                <div class="form-field">
+                  <div class="select-wrap">
+                    <div class="icon d-flex align-items-center justify-content-center">
+                      <b-icon-chevron-down></b-icon-chevron-down>
+                    </div>
+                    <select name="" id="" class="form-control">
+                      <option value="">Guest</option>
+                      <option value="">1</option>
+                      <option value="">2</option>
+                      <option value="">3</option>
+                      <option value="">4</option>
+                      <option value="">5</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="">
+              <div class="form-group">
+                <textarea name="" id="" cols="30" rows="2" class="form-control" placeholder="Message"></textarea>
+              </div>
+              <div class="form-group">
+                <input type="submit" value="I am attending" class="btn btn-primary py-3 px-4">
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  }
+})
+export default class RsvpSection extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/WeddingInfo.vue
+++ src/views/WeddingInfo.vue
@@ -21,56 +21,7 @@
 
     <when-where-section />
 
-    <section class="ftco-section bg-secondary" id="rsvp-section">
-      <div class="container">
-        <div class="row justify-content-center pb-5">
-          <div class="col-md-12 text-center heading-section ftco-animate">
-            <span class="clone">RSVP</span>
-            <h2 class="mb-3">Are Your Attending?</h2>
-          </div>
-        </div>
-        <div class="row justify-content-center">
-          <div class="col-md-7">
-            <form action="#" class="rsvp-form ftco-animate">
-              <div class="">
-                <div class="form-group">
-                  <input type="text" class="form-control" placeholder="Name">
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control" placeholder="Your email">
-                </div>
-              </div>
-              <div class="">
-                <div class="form-group">
-                  <div class="form-field">
-                    <div class="select-wrap">
-                      <div class="icon d-flex align-items-center justify-content-center"><span
-                          class="ion-ios-arrow-down"></span></div>
-                      <select name="" id="" class="form-control">
-                        <option value="">Guest</option>
-                        <option value="">1</option>
-                        <option value="">2</option>
-                        <option value="">3</option>
-                        <option value="">4</option>
-                        <option value="">5</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="">
-                <div class="form-group">
-                  <textarea name="" id="" cols="30" rows="2" class="form-control" placeholder="Message"></textarea>
-                </div>
-                <div class="form-group">
-                  <input type="submit" value="I am attending" class="btn btn-primary py-3 px-4">
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </section>
+    <rsvp-section />
 
     <section class="ftco-section" id="gallery-section">
       <div class="container-fluid px-md-4">
@@ -169,6 +120,7 @@ import LovestorySection from '@/components/LovestorySection.vue';
 import GreetingSection from '@/components/GreetingSection.vue';
 import PeopleSection from '@/components/PeopleSection.vue';
 import WhenWhereSection from '@/components/WhenWhereSection.vue';
+import RsvpSection from '@/components/RsvpSection.vue';
 
 @Component({
   components: {
@@ -181,7 +133,8 @@ import WhenWhereSection from '@/components/WhenWhereSection.vue';
     'lovestory-section': LovestorySection,
     'greeting-section': GreetingSection,
     'people-section': PeopleSection,
-    'when-where-section': WhenWhereSection
+    'when-where-section': WhenWhereSection,
+    'rsvp-section': RsvpSection
   }
 })
 export default class WeddingInfo extends Vue {
