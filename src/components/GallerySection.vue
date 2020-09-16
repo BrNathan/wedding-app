ac211256c3064d94f92cb7aaad28ecc56cf3e6c8
Nathan BRUET <nathan.bruet@gmail.com>
2020-09-16 08:05:36 +0200

    gallery section

---
+++ src/components/GallerySection.vue
@@ -0,0 +1,102 @@
+<template>
+  <section class="ftco-section" id="gallery-section">
+    <div class="container-fluid px-md-4">
+      <div class="row justify-content-center pb-5">
+        <div class="col-md-12 text-center heading-section">
+          <span class="clone">Photos</span>
+          <h2 class="mb-3">Gallery</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3">
+          <a href="images/gallery-1.jpg"
+            class="gallery img image-popup d-flex align-items-center justify-content-center"
+            style="background-image: url(images/gallery-1.jpg);">
+            <div class="icon d-flex align-items-center justify-content-center">
+              <b-icon-card-image></b-icon-card-image>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a href="images/gallery-2.jpg"
+            class="gallery img image-popup d-flex align-items-center justify-content-center"
+            style="background-image: url(images/gallery-2.jpg);">
+            <div class="icon d-flex align-items-center justify-content-center">
+              <b-icon-card-image></b-icon-card-image>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a href="images/gallery-3.jpg"
+            class="gallery img image-popup d-flex align-items-center justify-content-center"
+            style="background-image: url(images/gallery-3.jpg);">
+            <div class="icon d-flex align-items-center justify-content-center">
+              <b-icon-card-image></b-icon-card-image>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a href="images/gallery-4.jpg"
+            class="gallery img image-popup d-flex align-items-center justify-content-center"
+            style="background-image: url(images/gallery-4.jpg);">
+            <div class="icon d-flex align-items-center justify-content-center">
+              <b-icon-card-image></b-icon-card-image>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a href="images/gallery-5.jpg"
+            class="gallery img image-popup d-flex align-items-center justify-content-center"
+            style="background-image: url(images/gallery-5.jpg);">
+            <div class="icon d-flex align-items-center justify-content-center">
+              <b-icon-card-image></b-icon-card-image>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a href="images/gallery-6.jpg"
+            class="gallery img image-popup d-flex align-items-center justify-content-center"
+            style="background-image: url(images/gallery-6.jpg);">
+            <div class="icon d-flex align-items-center justify-content-center">
+              <b-icon-card-image></b-icon-card-image>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a href="images/gallery-7.jpg"
+            class="gallery img image-popup d-flex align-items-center justify-content-center"
+            style="background-image: url(images/gallery-7.jpg);">
+            <div class="icon d-flex align-items-center justify-content-center">
+              <b-icon-card-image></b-icon-card-image>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a href="images/gallery-8.jpg"
+            class="gallery img image-popup d-flex align-items-center justify-content-center"
+            style="background-image: url(images/gallery-8.jpg);">
+            <div class="icon d-flex align-items-center justify-content-center">
+              <b-icon-card-image></b-icon-card-image>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  }
+})
+export default class GallerySection extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/WeddingInfo.vue
+++ src/views/WeddingInfo.vue
@@ -23,82 +23,7 @@
 
     <rsvp-section />
 
-    <section class="ftco-section" id="gallery-section">
-      <div class="container-fluid px-md-4">
-        <div class="row justify-content-center pb-5">
-          <div class="col-md-12 text-center heading-section ftco-animate">
-            <span class="clone">Photos</span>
-            <h2 class="mb-3">Gallery</h2>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-3 ftco-animate">
-            <a href="images/gallery-1.jpg"
-              class="gallery img image-popup d-flex align-items-center justify-content-center"
-              style="background-image: url(images/gallery-1.jpg);">
-              <div class="icon d-flex align-items-center justify-content-center"><span class="ion-ios-image"></span>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-3 ftco-animate">
-            <a href="images/gallery-2.jpg"
-              class="gallery img image-popup d-flex align-items-center justify-content-center"
-              style="background-image: url(images/gallery-2.jpg);">
-              <div class="icon d-flex align-items-center justify-content-center"><span class="ion-ios-image"></span>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-3 ftco-animate">
-            <a href="images/gallery-3.jpg"
-              class="gallery img image-popup d-flex align-items-center justify-content-center"
-              style="background-image: url(images/gallery-3.jpg);">
-              <div class="icon d-flex align-items-center justify-content-center"><span class="ion-ios-image"></span>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-3 ftco-animate">
-            <a href="images/gallery-4.jpg"
-              class="gallery img image-popup d-flex align-items-center justify-content-center"
-              style="background-image: url(images/gallery-4.jpg);">
-              <div class="icon d-flex align-items-center justify-content-center"><span class="ion-ios-image"></span>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-3 ftco-animate">
-            <a href="images/gallery-5.jpg"
-              class="gallery img image-popup d-flex align-items-center justify-content-center"
-              style="background-image: url(images/gallery-5.jpg);">
-              <div class="icon d-flex align-items-center justify-content-center"><span class="ion-ios-image"></span>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-3 ftco-animate">
-            <a href="images/gallery-6.jpg"
-              class="gallery img image-popup d-flex align-items-center justify-content-center"
-              style="background-image: url(images/gallery-6.jpg);">
-              <div class="icon d-flex align-items-center justify-content-center"><span class="ion-ios-image"></span>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-3 ftco-animate">
-            <a href="images/gallery-7.jpg"
-              class="gallery img image-popup d-flex align-items-center justify-content-center"
-              style="background-image: url(images/gallery-7.jpg);">
-              <div class="icon d-flex align-items-center justify-content-center"><span class="ion-ios-image"></span>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-3 ftco-animate">
-            <a href="images/gallery-8.jpg"
-              class="gallery img image-popup d-flex align-items-center justify-content-center"
-              style="background-image: url(images/gallery-8.jpg);">
-              <div class="icon d-flex align-items-center justify-content-center"><span class="ion-ios-image"></span>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
+    <gallery-section />
 
     <!-- footer -->
     <footer-page></footer-page>
@@ -121,6 +46,7 @@ import GreetingSection from '@/components/GreetingSection.vue';
 import PeopleSection from '@/components/PeopleSection.vue';
 import WhenWhereSection from '@/components/WhenWhereSection.vue';
 import RsvpSection from '@/components/RsvpSection.vue';
+import GallerySection from '@/components/GallerySection.vue';
 
 @Component({
   components: {
@@ -134,7 +60,8 @@ import RsvpSection from '@/components/RsvpSection.vue';
     'greeting-section': GreetingSection,
     'people-section': PeopleSection,
     'when-where-section': WhenWhereSection,
-    'rsvp-section': RsvpSection
+    'rsvp-section': RsvpSection,
+    'gallery-section': GallerySection
   }
 })
 export default class WeddingInfo extends Vue {
